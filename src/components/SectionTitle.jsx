9e0e0564032a28a63cb382156eb9c026d85e1f7d
Bright Dikko <bdikko@nd.edu>
2023-02-26 21:18:45 -0500

    Added Web Responsiveness"
    "

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 const SectionTitle = (props) => {
-    console.log(props.title);
     return (
         <div className=" py-40 px-16 mx-auto">
             <p className="text-center font-serif mb-20 font-extrabold text-black text-3xl ">
